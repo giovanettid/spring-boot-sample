fe5f3f96c98ab78910ca44d82b65523219d193af
didier <didier.giovanetti@gmail.com>
2015-06-11 21:36:12 +0200

    add mysql dependency

--- src/main/java/com/giovanetti/Personne.java
+++ src/main/java/com/giovanetti/Personne.java
@@ -2,13 +2,11 @@ package com.giovanetti;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "USER")
 public class Personne {
 
     private Personne() {}
@@ -19,13 +17,16 @@ public class Personne {
     }
 
     @Id
+    @Column(name = "ID")
     @GeneratedValue(strategy= GenerationType.AUTO)
     @JsonIgnore
     private long id;
 
+    @Column(name = "PRENOM")
     private String prenom;
 
     @Size(min=2)
+    @Column(name = "NOM")
     private String nom;
 
     public String getPrenom() {
